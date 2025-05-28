c3cc88f30eebedd3008df90b6707986a5e259bca
Putin <jportelagarcia@danielcastelao.org>
2025-05-28 13:55:23 +0200

    Empecé a codificar

--- src/Coche.java
+++ src/Coche.java
@@ -1,15 +1,20 @@
 package MVC_coches.src;
-//
+
 public class Coche {
-    String matricula;
-    String modelo;
-    Integer velocidad;
+   private String matricula;
+   private String modelo;
+   private Integer velocidad;
+   private Double gasolina;
+   private Double posicion;
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+        this.gasolina = 0.0;
+        this.posicion = 0.0;
     }
+
 
     public String getMatricula() {
         return matricula;
@@ -33,5 +38,21 @@ public class Coche {
 
     public void setVelocidad(Integer velocidad) {
         this.velocidad = velocidad;
+    }
+
+    public Double getGasolina() {
+        return gasolina;
+    }
+
+    public void setGasolina(Double gasolina) {
+        this.gasolina = gasolina;
+    }
+
+    public Double getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Double posicion) {
+        this.posicion = posicion;
     }
 }
